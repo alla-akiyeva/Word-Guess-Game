f3754f41a56f1d2c40c20ac510c365ea92273fd2
Alla Akiyeva <alla.akiyeva@gmail.com>
2019-03-18 15:46:49 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,77 +1,76 @@
 // Creating an array with word options
 var wordOptionsList = [
-    "Cuttlefish",
-    "Shrimp",
-    "Octopus",
-    "Squid",
-    "Scallops",
-    "Crab",
-    "Cthulhu",
-    "Salmon",
-    "Shark",
-    "Tilapia",
-    "Tuna",
-    "Lobster",
-    "Catfish",
-    "Anchovy",
-    "Herring",
-    "Flounder",
-    "Caviar",
-    "Clam",
-    "Sturgeon",
-    "Oyster",
-    "Mussel"
+    "cuttlefish",
+    "shrimp",
+    "octopus",
+    "squid",
+    "scallops",
+    "crab",
+    "cthulhu",
+    "salmon",
+    "shark",
+    "tilapia",
+    "tuna",
+    "lobster",
+    "catfish",
+    "anchovy",
+    "herring",
+    "flounder",
+    "caviar",
+    "clam",
+    "sturgeon",
+    "oyster",
+    "mussel"
 ];
 
-// Creating variables to store wins, guesses remaining, and letters used. 
-var wins = 0;
-var guessesRemaining = 10;
+// Creating variables  
+var guessesRemaining = 12;
 var wrongGuesses = [];
 var blankArray = [];
+var wordChosen = "";
 
 // Creating variables that hold references to the places in the HTML where we want to display things.
-var winsText = document.getElementById("wins-text");
 var currentWordText = document.getElementById("current-word-text");
-var placeHolder = document.getElementById("word-place-holder");
+var wordChosenText = document.getElementById("word-place-holder");
 var guessesText = document.getElementById("guesses-text");
 var wrongGuessesText = document.getElementById("wrong-guesses");
 
+// Choosing a random word from the options array and generating an array for "_ _ _ _"
 var wordChosen = wordOptionsList[Math.floor(Math.random() * wordOptionsList.length)];
 console.log(wordChosen);
 for (var i = 0; i < wordChosen.length; i++) {
-    blankArray[i] = "_";
-    placeHolder.textContent = blankArray.join(" ");
+        blankArray[i] = "_";
+        wordChosenText.textContent = blankArray.join(" ");
 }
 
 var lettersRemaining = wordChosen.length;
-console.log(lettersRemaining);
-
-//while (lettersRemaining > 0) {
-document.onkeyup = function(event) {
-//* Determines which key is pressed: 
+document.onkeyup = function(event) { 
 var letterGuessed = event.key;
-console.log(letterGuessed);
-//* splits wordChosen into array: 
-var text = wordChosen.split('');
-console.log(text);
 
+do {
 for (var j = 0; j < wordChosen.length; j++) {
     if (letterGuessed === wordChosen[j]) {
         blankArray[j] = letterGuessed;
-        placeHolder.textContent = blankArray.join(" ");
-        lettersRemaining--;
-        console.log(lettersRemaining);
-        found = true;
+        wordChosenText.textContent = blankArray.join(" ");
     }
 }   
     if (blankArray[j] !== letterGuessed && blankArray.includes(letterGuessed)===false) {
         wrongGuesses.push(letterGuessed);
-        
         guessesRemaining--;
+
+        wrongGuessesText.textContent = "Letters guessed wrong: " + wrongGuesses.join(" ");
         guessesText.textContent = "Number of Guesses Remaining: " + guessesRemaining;
+       }
+       if (guessesRemaining === 0) {
+               alert("You lost!");
+       }
+       if (blankArray.includes("_")===false) {
+               alert("You won!!! The word is " + wordChosen);
+               location.reload()
+       }
 }
+while (guessesRemaining < 0);
 }
-//}
-wins++;
-winsText.textContent = "Wins: " + wins;
+
+
 
